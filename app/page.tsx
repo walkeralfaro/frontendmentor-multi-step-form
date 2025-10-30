352c7e2934dedbe87d1983a1d3ff6910e7e1f8f6
walker <dev.walkeralfaro@gmail.com>
2025-10-29 20:31:07 -0500

    fix bg imagen size

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
 
-      <div className="container mx-auto max-w-5xl bg-gray-100 bg-[url(/bg-sidebar-mobile.svg)] bg-no-repeat">
+      <div className="container mx-auto max-w-5xl bg-gray-100 bg-[url(/bg-sidebar-mobile.svg)] bg-no-repeat bg-contain">
 
         <FormComponent />
       </div>
